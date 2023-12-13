24f004baf4164a38b168cf6426d2bfb195381ff9
komafuji820 <tfukae25@gmail.com>
2023-12-14 00:46:44 +0900

    ニックネームに文字制限を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 10 }
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
+    nickname {Faker::Name.initials(number: 10)}
     email {Faker::Internet.email}
     password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}

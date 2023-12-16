b5771f0ed503431f369d8d40b0ca6997d574029d
komafuji820 <tfukae25@gmail.com>
2023-12-16 14:58:47 +0900

    グループ作成画面の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,20 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+  
   def index
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def group_params
+  end
+
 end

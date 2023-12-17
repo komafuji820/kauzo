73a5418ee5052773530a2ec2e889306353f26d47
komafuji820 <tfukae25@gmail.com>
2023-12-17 12:44:05 +0900

    グループ作成時のメッセージを追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,8 +14,10 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
+      flash[:group_create] = "グループ#{@group.name}を作成しました。"
       redirect_to root_path
     else
+      flash.now[:group_name_error] = "グループ名を入力してください。"
       render :new, status: :unprocessable_entity
     end
   end

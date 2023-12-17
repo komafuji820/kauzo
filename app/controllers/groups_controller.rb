2b94b38f2456c0e60bde4eb30f72d010a7ce185b
komafuji820 <tfukae25@gmail.com>
2023-12-17 11:34:37 +0900

    グループ作成機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @groups = Group.all
   end
 
   def new
@@ -9,12 +10,18 @@ class GroupsController < ApplicationController
   end
 
   def create
-    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-
+  
   private
 
   def group_params
+    params.require(:group).permit(:name)
   end
 
 end

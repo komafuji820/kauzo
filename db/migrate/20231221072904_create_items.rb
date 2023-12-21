65e7c4f0403ecd2175243b88dee3d64740a0f5f8
komafuji820 <tfukae25@gmail.com>
2023-12-22 00:39:30 +0900

    itemの外部キーにcategoryを追加

--- db/migrate/20231221072904_create_items.rb
+++ db/migrate/20231221072904_create_items.rb
@@ -1,8 +1,9 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.text :memo
+      t.string :memo
       t.references :group, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.timestamps
     end
   end

8b73f2e2f523b2c364e7918146c1bf3bcfabb7b5
komafuji820 <tfukae25@gmail.com>
2023-12-22 00:39:12 +0900

    categoryテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_113115) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_152458) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_113115) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "group_users", charset: "utf8", force: :cascade do |t|
     t.bigint "group_id", null: false
     t.bigint "user_id", null: false
@@ -55,10 +61,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_113115) do
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
-    t.text "memo"
+    t.string "memo"
     t.bigint "group_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["group_id"], name: "index_items_on_group_id"
   end
 
@@ -79,5 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_113115) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "items", "categories"
   add_foreign_key "items", "groups"
 end

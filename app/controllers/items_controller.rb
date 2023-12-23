a24f424d0480a0cf915c87369ca9d5154de356d8
komafuji820 <tfukae25@gmail.com>
2023-12-23 12:16:50 +0900

    在庫削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to group_items_path(id: item.group.id)
+  end
+
   private
 
   def item_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       # メンバーをリストに追加する
       get 'add_members_to_list'
     end
-    resources :items, only: [:index, :new, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
   end
   
   resources :users do
